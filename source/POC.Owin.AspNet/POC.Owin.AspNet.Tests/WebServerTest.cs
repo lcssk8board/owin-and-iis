bf95710705be319a1faf084a03597aebbcb85d25
lcssk8board <lucas.ce.bertin@gmail.com>
2016-04-28 08:29:10 -0300

    TestServer in-memory

---
+++ source/POC.Owin.AspNet/POC.Owin.AspNet.Tests/WebServerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Testing;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace POC.Owin.AspNet.Tests
+{
+    public class WebServerTest : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public WebServerTest()
+        {
+            _server = TestServer.Create<Startup>();
+        }
+
+        public void Dispose()
+        {
+            _server.Dispose();
+        }
+
+
+        [Fact]
+        public void Test()
+        {
+            var response = _server.HttpClient.GetAsync("/api/test").Result;
+            var result = response.Content.ReadAsAsync<int>().Result;
+            Assert.Equal(1, result);
+        }
+    }
+}
